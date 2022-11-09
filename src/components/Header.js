38fcde02d448f9bfef380ef145d6d1ecdbb03b96
Enzo Fernandez <enzoabel1@gmail.com>
2022-11-09 13:26:21 -0300

    terminado

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useContext} from "react";
 import { DataContext } from "context/DataProvider";
 import { Link } from "react-router-dom";
 import Nike from "images/Nike.jpg";
-
+import "./Header.css";
 
 export const Header = () => {
   const value = useContext(DataContext);
@@ -10,33 +10,33 @@ export const Header = () => {
   const [menu, setMenu] = value.menu;
 
 
-  const toogleMenu = () =>{
+  const toggleMenu = () =>{
     setMenu(!menu)
   }
  
 
   return (
-    <header>
-      <div className="menu">
-      <box-icon name="menu"></box-icon>
+    <nav className="navbar navbar-expand-lg bg-light fixed-top">
+    <div className="container-fluid">
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul className="navbar-nav mb-2 fs-5 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link" to={"/"}>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={"/productos"}>Productos</Link>
+          </li>
+          <li className="nav-item cart" onClick={toggleMenu}>
+            <box-icon name="cart"></box-icon>
+            <span className="item__total"> {carrito.length} </span>
+          </li>
+        </ul>
       </div>
-      <Link to="/">
-      <div className="logo">
-        <img src={Nike} alt="Nike" width="150" />
-      </div>
-      </Link>
-      <ul>
-        <li>
-          <Link to="/">INICIO</Link>
-        </li>
-        <li>
-          <Link to="/productos">PRODUCTOS</Link>
-        </li>
-      </ul>
-      <div className="cart" onClick={toogleMenu}>
-        <box-icon name="cart"></box-icon>
-        <span className="item__total"> {carrito.length} </span>
-      </div>
-    </header>
+    </div>
+  </nav>
   );
 };
